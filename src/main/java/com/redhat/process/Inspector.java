e1271bcc417db11f5c20c6d3b19fc68e28fecd33
hinunbi <hinunbi@gmail.com>
2018-08-14 14:43:55 +0900

    수정

--- src/main/java/com/redhat/process/Inspector.java
+++ src/main/java/com/redhat/process/Inspector.java
@@ -5,6 +5,7 @@ import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import sun.misc.HexDumpEncoder;
 
 @Component
 public class Inspector implements Processor {
@@ -13,6 +14,14 @@ public class Inspector implements Processor {
 
   @Override
   public void process(Exchange exchange) throws Exception {
+
+    byte[] in = exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class,
+        exchange.getIn().getBody());
+
+    HexDumpEncoder hexDump = new HexDumpEncoder();
+
+    logger.info("Sending body dump :\n" + hexDump.encodeBuffer(in));
+
     logger.info("body : " + exchange.getIn().getBody());
   }
 }
